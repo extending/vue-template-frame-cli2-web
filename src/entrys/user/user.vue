109b81de5a00c7f13ac843fc01f31275b1dfa9ac
吕瑶 <lvyao91@sina.com>
2019-04-21 14:02:37 +0800

    添加多页面 user.html，该页面无路由功能

---
+++ src/entrys/user/user.vue
@@ -0,0 +1,21 @@
+<template lang="pug">
+.user
+  h1 多页面三-user
+</template>
+
+<script>
+export default {
+  name: 'user'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
