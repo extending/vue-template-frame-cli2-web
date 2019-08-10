6b9e8041dc05ad5d73a576cc8e183de34ea14a07
吕瑶 <lvyao91@sina.com>
2019-08-11 01:17:29 +0800

    更新 vuex mapState mapGetters

--- src/pages/index/store.js
+++ src/pages/index/store.js
@@ -4,10 +4,22 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
+  strict: process.env.NODE_ENV !== 'production',
   state: {
 		user: null,
 		mobile: '',
-		authed: ''
+    authed: '',
+    count1: 10,
+    count2: 20,
+  },
+  getters: {
+    totalCount(state) {
+      return state.count1 + state.count2
+    },
+    // store 的计算属性返回一个函数，即可接受参数
+    increaseCount1: (state) => (val) => {
+      return state.count1 + val;
+    }
   },
   mutations: {
     user(state,user) {
