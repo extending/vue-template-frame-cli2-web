564bf4675e629b6329bcda47b374a1dbfabc3936
吕瑶 <lvyao91@sina.com>
2019-08-26 09:13:51 +0800

    test Vue.nextTick()

--- src/pages/index/views/pages/demos/c-c.vue
+++ src/pages/index/views/pages/demos/c-c.vue
@@ -1,6 +1,8 @@
 <template lang='pug'>
 div
-  h3 组件 c-c：这是兄弟组件 c-b 传来的数据：{{getEventBus}}
+  //- h3 组件 c-c：这是兄弟组件 c-b 传来的数据：{{getEventBus}}
+  h3 {{testNextTickMessage}}
+  el-button(@click="changeMessage")
 </template>
 
 <script>
@@ -8,12 +10,21 @@ export default {
   name: 'c-c',
   data () {
     return {
-      getEventBus: ''
+      getEventBus: '',
+      testNextTickMessage: ''
     };
   },
   components: {},
   computed: {},
-  methods: {},
+  methods: {
+    changeMessage() {
+      this.testNextTickMessage = "message";
+      console.log(this.$el.textContent === "message"); // false
+      this.$nextTick(() => {
+        console.log(this.$el.textContent === "message"); // true
+      })
+    }
+  },
   mounted() {
     this.$EventBus.$on('globalEvent', (val) => {
       console.log('val ', val)
